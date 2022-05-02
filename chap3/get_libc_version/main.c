17b2ead79146b9f8a8c76128d83ba6c52b95ab9c
Khai Pham <khaipham9624@gmail.com>
2022-05-02 17:37:05 +0700

    edit get_libc_version

--- chap3/get_libc_version/main.c
+++ chap3/get_libc_version/main.c
@@ -3,19 +3,5 @@
 #include "tlpi_hdr.h"
 int main(int argc, char **argv)
 {
-    if (argc > 1)
-    {
-        printf("argv[1] = %d\n", getInt(argv[1], GN_ANY_BASE, "opt"));
-    }
-
-    // switch (expression)
-    // {
-    // case /* constant-expression */:
-    //     /* code */
-    //     break;
-    
-    // default:
-    //     break;
-    // }
     printf("libc version: %s\n", get_libc_version());
 }
