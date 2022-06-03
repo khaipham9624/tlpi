24746ce912d9394756daa4b9fe6e56a0b28556b3
Khai Pham <khaipham9624@gmail.com>
2022-06-03 12:19:47 +0700

    update lib

--- lib/error_functions.h
+++ lib/error_functions.h
@@ -1,6 +1,7 @@
 #ifndef ERROR_FUNCTIONS_H
 #define ERROR_FUNCTIONS_H
 
+// prints a message corresponding to errno on standard error (with newline character)
 void errMsg(const char *format, ...);
 
 #ifdef __GNUC__
@@ -10,10 +11,16 @@ void errMsg(const char *format, ...);
 #define NORETURN
 #endif
 
+// prints a message corresponding to errno then if EF_DUMPCORE, call abort() else exit().
 void errExit(const char* format, ...) NORETURN;
+// prints a message corresponding to errno then if EF_DUMPCORE, call abort() else _exit().
 void err_exit(const char* format, ...) NORETURN;
+// prints a message corresponding to errnum then if EF_DUMPCORE, call abort() else exit().
 void errExitEN(int errnum, const char* format, ...) NORETURN;
+// prints a message then f EF_DUMPCORE, call abort() else exit().
 void fatal(const char* format, ...) NORETURN;
+// prints a message to help user know usage
 void usageErr(const char* format, ...) NORETURN;
+// prints a message to help command line
 void cmdLineErr(const char* format, ...) NORETURN;
 #endif
